00b71fe44f24a3508be6f0f24719e2e4834d2ebc
vidur.tukatech <vidurp@tukatech.com>
2020-06-26 10:43:29 +0530

    added add and remove functionality

--- app/javascript/components/Burger/BuildControls/BuildControls.js
+++ app/javascript/components/Burger/BuildControls/BuildControls.js
@@ -3,17 +3,23 @@ import classes from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    { label: 'Salad', type: 'salad' },
-    { label: 'Bacon', type: 'bacon' },
-    { label: 'Cheese', type: 'cheese' },
-    { label: 'Meat', type: 'meat' }
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
 ];
 
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                type={ctrl.type}
+                added={() => props.ingredientAdded(ctrl.type)}
+                remove={() => props.ingredientRemove(ctrl.type)}
+            />
         ))}
     </div>
 );
